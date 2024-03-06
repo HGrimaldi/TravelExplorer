5cd3fc695cee2ead51ddf12ca2ea8286a092f2d3
Brian Alexander Fuentes Lazo <62920279+AlexLazo@users.noreply.github.com>
2024-03-06 16:53:41 -0600

    Cambios secundarios

--- Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccf7f9b58e09fa5f6e5b778ebb9a95bded9ff5c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06add1622c5312d55fe370984e26c9aca18490d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
