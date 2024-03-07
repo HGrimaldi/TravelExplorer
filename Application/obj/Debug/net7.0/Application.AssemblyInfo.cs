3cdae1d408f67b195987f8c33e66cde49d856af2
HGrimaldi <hgrimaldi15@gmail.com>
2024-03-07 12:33:10 -0600

    actualizacion

--- Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3385ec4dacf41607528feaae5487f5dcf4d0eca4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5916aa823b35b5053d6372a44ffa6f30802aca43")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
